98bf8d41c2f191e7381e0c96ed1ccefaac814b08
yefeng <530030302@qq.com>
2020-09-02 10:28:43 +0800

    1.添加官方SDK LED灯实验

---
+++ led_sdk/cc.h
@@ -0,0 +1,25 @@
+#ifndef __CC_H__
+#define __CC_H__
+
+#define     __I     volatile
+#define     __O     volatile
+#define     __IO    volatile
+
+#define     ON  1
+#define     OFF 0
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long int s64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u33;
+
+#endif
